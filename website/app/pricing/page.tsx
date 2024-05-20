8e1f6cc81ae1305f2d57ef8adbe8438343fe5e55
Aykut Gedik <aykutged@gmail.com>
2024-05-19 23:50:35 -0700

    website changes

---
+++ website/app/pricing/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <div className="w-full text-center mt-20 text-2xl">We're working on a developer-friendly pricing model. Stay tuned!
+        </div>
+    )
+}
