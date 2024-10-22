5e676b5aa031fa36fa9ad37e51cc59b944ca312d
Ferhat Korkmaz <ferhatkorkmaz@email.com>
2024-10-22 16:15:09 +0300

    docker and render guides

--- examples/nextjs/next.config.mjs
+++ examples/nextjs/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //output: 'standalone', // For web services/Docker
   experimental: {
     instrumentationHook: true, // To enable instrumentation
   },
